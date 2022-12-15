f393ddd872cad2c7113cb3469274107e90b49638
ferter3006 <ferratersimon@gmail.com>
2022-12-15 14:06:41 +0100

    flipes

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ import { PantallaFinal } from './components/PantallaFinal/PantallaFinal';
 export default function App() {
 
   return (
-    /*<div className='container'>
+    <div className='container'>
       <header>
         <h1>Reus<span className='de'>de</span>Reus</h1>
-        <p className='byferter'>by Ferter / versió Beta 0.6</p>
+        <p className='byferter'>by Ferter / versió Beta 0.7</p>
         <TeamInputs />
       </header>
-      <TeamInputs />*/
+      <TeamInputs />
       <BrowserRouter>
         <Routes>
           <Route path='/reusdereus' element={<TeamInputs />} />
@@ -31,7 +31,7 @@ export default function App() {
         </Routes>
       </BrowserRouter>
 
-    /*</div>*/
+    </div>
   );
 }
 
